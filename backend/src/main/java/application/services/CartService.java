3ff40997ac956c03be7c05260661f0b38927ce22
Matteo Orlando <101630852+teor0@users.noreply.github.com>
2024-02-20 18:22:14 +0000

    remove old commented checkout method

--- backend/src/main/java/application/services/CartService.java
+++ backend/src/main/java/application/services/CartService.java
@@ -107,31 +107,4 @@ public class CartService {
         c.setOrder(null);
         repo.save(c);
     }
-
-
-
-   /* @Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = {UtenteNotExistsException.class, QtaUnvaliableException.class, PriceChangedException.class}, propagation = Propagation.REQUIRED)
-    public void acquire(UtenteDTO dto) throws QtaUnvaliableException, PriceChangedException, OrderNotExistsException, UtenteNotExistsException {
-        if(!urepo.existsById(dto.getId()))
-            throw new UtenteNotExistsException();
-        Utente u = urepo.findById(dto.getId()).get();
-        Cart c = repo.findByUser(u).get();
-        if(!orepo.existsById(c.getOrder().getId()))
-            throw new OrderNotExistsException();
-        Order o = orepo.findById(c.getOrder().getId()).get();
-        List <OrderProducts> items = o.getOrderProducts();
-        for(OrderProducts item: items){
-            Product p= prepo.findById(item.getProduct().getId()).get();
-            if(p.getQuantity()<item.getQuantity())
-                throw new QtaUnvaliableException();
-            if(!p.getPrice().equals(item.getPrice()))
-                throw new PriceChangedException();
-            p.setQuantity(p.getQuantity()- item.getQuantity());
-            prepo.save(p);
-        }
-        //payment...
-        o.setStatus("Handled");
-        c.setOrder(null);
-        repo.save(c);
-    }*/
 }
